f8a436cdb98367f9730d5dce845fc9569d1f25da
TripatjotSingh19 <tripatjots646@gmail.com>
2025-08-03 19:27:15 +0530

    Create enter and display a string.cpp

---
+++ enter and display a string.cpp
@@ -0,0 +1,24 @@
+// Tripatjot singh chawla
+// 24070123044
+// Entc a2
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+
+    cout << "Enter a string: ";
+    cin >> str;
+
+    cout << "You entered: " << str << endl;
+
+    return 0;
+}
+
+/* OUTPUT
+
+Enter a string: Hello
+You entered: Hello
+*/
